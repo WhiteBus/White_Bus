77479f2c03fd96c4fae2e3d4fe136418ecc558bf
taegyukim <sa090180@gmail.com>
2024-05-25 19:34:23 +0900

    즐겨찾기 이름기반 좌표 검색 구현

--- app/src/main/java/com/google/mediapipe/examples/facelandmarker/favorite/DestinationViewHolder.kt
+++ app/src/main/java/com/google/mediapipe/examples/facelandmarker/favorite/DestinationViewHolder.kt
@@ -2,10 +2,22 @@ package com.google.mediapipe.examples.facelandmarker.favorite
 
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.google.mediapipe.examples.facelandmarker.R
 
-class DestinationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val textViewName: TextView = itemView.findViewById(R.id.textViewName)
-    val textViewAddress: TextView = itemView.findViewById(R.id.textViewAddress)
+class DestinationViewHolder(itemView: View, private val itemClickListener: (String) -> Unit) :
+    RecyclerView.ViewHolder(itemView) {
+
+    val textViewName: TextView = itemView.findViewById(R.id.destinationName)
+    val textViewAddress: TextView = itemView.findViewById(R.id.destinationAddress)
+
+    init {
+        itemView.setOnClickListener {
+            val context = itemView.context
+            val destinationName = textViewName.text.toString()
+            Toast.makeText(context, "Clicked on: $destinationName", Toast.LENGTH_SHORT).show()
+//           여기다 즐겨찾기 검색 후 과정 추가
+        }
+    }
 }
--- app/src/main/java/com/google/mediapipe/examples/facelandmarker/repository/DestinationRepository.kt
+++ app/src/main/java/com/google/mediapipe/examples/facelandmarker/repository/DestinationRepository.kt
@@ -3,13 +3,14 @@ package com.google.mediapipe.examples.facelandmarker.repository
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import com.google.mediapipe.examples.facelandmarker.database.DestinationContract
 import com.google.mediapipe.examples.facelandmarker.database.DestinationDbHelper
 
 class DestinationRepository(context: Context) {
 
     private val dbHelper = DestinationDbHelper(context)
-    private val db = dbHelper.writableDatabase
+    private val db: SQLiteDatabase = dbHelper.writableDatabase
 
     fun insertDestination(name: String, xCoord: Double, yCoord: Double, address: String): Long {
         val values = ContentValues().apply {
@@ -26,6 +27,32 @@ class DestinationRepository(context: Context) {
             DestinationContract.DestinationEntry.TABLE_NAME,
             null, null, null, null, null, null
         )
+    }
+
+    fun getDestinationCoordinatesByName(name: String): Pair<Double, Double>? {
+        val cursor = db.query(
+            DestinationContract.DestinationEntry.TABLE_NAME,
+            arrayOf(
+                DestinationContract.DestinationEntry.COLUMN_NAME_X_COORD,
+                DestinationContract.DestinationEntry.COLUMN_NAME_Y_COORD
+            ),
+            "${DestinationContract.DestinationEntry.COLUMN_NAME_NAME} = ?",
+            arrayOf(name),
+            null, null, null
+        )
+
+        var coordinates: Pair<Double, Double>? = null
+
+        with(cursor) {
+            if (moveToFirst()) {
+                val xCoord = getDouble(getColumnIndexOrThrow(DestinationContract.DestinationEntry.COLUMN_NAME_X_COORD))
+                val yCoord = getDouble(getColumnIndexOrThrow(DestinationContract.DestinationEntry.COLUMN_NAME_Y_COORD))
+                coordinates = Pair(xCoord, yCoord)
+            }
+            close()
+        }
+
+        return coordinates
     }
 
     fun close() {
